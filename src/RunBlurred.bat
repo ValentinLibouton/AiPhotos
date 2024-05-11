b8b052b5e9f8ea48b60938ba587ed8981f982a79
valentinlibouton <valentin.libouton.git.des8s@8shield.net>
2024-05-11 22:15:02 +0200

    debug batch file windows, blank space in path

--- src/RunBlurred.bat
+++ src/RunBlurred.bat
@@ -1,3 +1,5 @@
 @echo off
-python "C:\Users\destr\OneDrive\Documents\blurred.py" %*
+echo Veuillez entrer le chemin complet du dossier a analyser:
+set /p dir_path="Chemin: "
+python "C:\Users\destr\OneDrive\Documents\AiPhotos\blurred.py" "%dir_path%"
 pause
